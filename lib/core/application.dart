d2160274480867c37ce52178f1ce761d69577777
Vladimir <vovaklh20012@gmail.com>
2022-03-29 16:14:25 +0300

    feat: Setup localization

---
+++ lib/core/application.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/l10n/l10n.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:products_app/presentation/pages/products_page.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Products',
+      supportedLocales: L10n.all,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: const ProductsPage(),
+    );
+  }
+}
---
+++ lib/presentation/pages/products_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductsPage extends StatelessWidget {
+  const ProductsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(color: Colors.red),
+      ),
+    );
+  }
+}
